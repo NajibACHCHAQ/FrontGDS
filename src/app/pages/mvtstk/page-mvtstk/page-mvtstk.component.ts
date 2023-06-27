4a6b86f13c859a12728f137580f58b735b5a2c51
ACHCHAQ <najib.achchaq@gmail.com>
2023-06-28 01:03:45 +0200

    ajout d'un background sur les accordéon cliquer + mise en forme other

--- src/app/pages/mvtstk/page-mvtstk/page-mvtstk.component.ts
+++ src/app/pages/mvtstk/page-mvtstk/page-mvtstk.component.ts
@@ -1,17 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-mvtstk',
   templateUrl: './page-mvtstk.component.html',
   styleUrls: ['./page-mvtstk.component.scss']
 })
-export class PageMvtstkComponent {
+export class PageMvtstkComponent implements OnInit {
+  mvtstk: any[] = [
+    {
+      title: 'Accordeon 1',
+      details: ['Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 2',
+      details: ['Détail 4', 'Détail 5', 'Détail 6']
+    },
+    {
+      title: 'Accordeon 3',
+      details: ['Détail 7', 'Détail 8', 'Détail 9']
+    },
+    {
+      title: 'Accordeon 1',
+      details: ['Détail 1', 'Détail 2', 'Détail 3', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 2',
+      details: ['Détail 4', 'Détail 5', 'Détail 6', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 3',
+      details: ['Détail 7', 'Détail 8', 'Détail 9', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 1',
+      details: ['Détail 1', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 2',
+      details: ['Détail 4', 'Détail 5', 'Détail 6', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 3',
+      details: ['Détail 7', 'Détail 8', 'Détail 9', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 1',
+      details: ['Détail 1', 'Détail 2', 'Détail 3', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 2',
+      details: ['Détail 4', 'Détail 5', 'Détail 6', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    },
+    {
+      title: 'Accordeon 3',
+      details: ['Détail 7', 'Détail 8', 'Détail 9', 'Détail 2', 'Détail 3','Détail 1', 'Détail 2', 'Détail 3']
+    }
+  ];
+  activeIndex: number = -1;
 
-  mvtstk = [{},];
+  constructor() { }
 
-  activeIndex: number | null = null;
+  ngOnInit() {
+  }
 
-  toggleAccordion(index: number): void {
-    this.activeIndex = this.activeIndex === index ? null : index;
+  toggleAccordion(index: number) {
+    if (this.activeIndex === index) {
+      this.activeIndex = -1;
+    } else {
+      this.activeIndex = index;
+    }
   }
 }
